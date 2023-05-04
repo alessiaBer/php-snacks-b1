86d8c7d838bf0dcf964f022b034cbdf3008ad78e
alessiaBer <alexdeyn@gmail.com>
2023-05-04 12:30:02 +0200

    [add] snack 1

--- snack1/index.php
+++ snack1/index.php
@@ -36,13 +36,43 @@ $basketMatches = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <h1 class="text-center py-4">NBA matches</h1>
+    <div class="container-fluid d-flex flex-column aligm-items-center">
+        <?php foreach ($basketMatches as $match) : ?>
+        <div class="card rounded-0 shadow col-8 my-0 mx-auto">
+            <div class="card-body">
+                <div class="home d-flex">
+                    <h3 class="pe-4">
+                        <strong>Home Team:</strong> <?= $match["homeTeam"] ?>
+                    </h3>
+                    <span class="fs-4 text-primary">
+                        <?= $match["homePoints"] ?>
+                    </span>
+                </div>
+                <div class="guests d-flex">
+                    <h3 class="pe-4">
+                        <strong>Guest Team:</strong> <?= $match["guestTeam"] ?>
+                    </h3>
+                    <span class="fs-4 text-primary">
+                        <?= $match["guestPoints"] ?>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <?php endforeach ?>
+    </div>
+
 </body>
+
 </html>
