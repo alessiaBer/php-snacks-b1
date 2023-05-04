4959108cb3b40af0a67521338da8b1f86fb3e619
alessiaBer <alexdeyn@gmail.com>
2023-05-04 13:01:31 +0200

    [try]

--- snack2/index.php
+++ snack2/index.php
@@ -6,7 +6,18 @@ che mail contenga un punto e una chiocciola e
 che age sia un numero. 
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 */
+$name = ($_GET["name"]);
+$email = ($_GET["email"]);
+$age = ($_GET);
+$characters = ['@', '.'];
 
+$validName = (strlen($name) > 3);
+$validEmail = str_contains($email, $characters);
+$validAge = is_numeric($age);
+
+/* if ($name === $validName && $email === $validEmail && $age === $validAge) {
+    echo 'ciao';
+}; */
 ?>
 
 <!DOCTYPE html>
@@ -17,10 +28,27 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
-
+    <div class="container d-flex-justify-content-center my-4">
+        <form method="get" class="col-8 my-0 mx-auto">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Type your name">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Type your email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Age</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Type your age">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </body>
 
 </html>
